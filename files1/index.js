368e2fded1dc0c50e59ea153f528aa984eeb503f
Andrew Butler <andrew@configit.com>
2019-10-16 12:36:48 +0100

    Add files

---
+++ files1/index.js
@@ -0,0 +1,21 @@
+module.exports = reviewBot
+
+const sendLogs = require( './lib/logs/send' )
+const handlePullRequestChange = require( './lib/handle-pull-request-change' )
+const handleRequestedAction = require( './lib/handle-requested-action' )
+
+function reviewBot( app ) {
+  // listen to all relevant pull request event actions
+  app.on( [
+    'pull_request.opened',
+    'pull_request.edited',
+    'pull_request.labeled',
+    'pull_request.unlabeled',
+    'pull_request.synchronize'
+  ], handlePullRequestChange.bind( null, app ) )
+
+  // listen to an overwrite request action from a check run
+  app.on( 'check_run.requested_action', handleRequestedAction.bind( null, app ) )
+
+  sendLogs(app)
+}
